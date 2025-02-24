9e08f66724f2d528d3b05178cc9fc0b29f842edb
Nebojsa Trajkovic <55205314+nebojsatrajkovic@users.noreply.github.com>
2025-02-24 13:48:18 +0100

    pdf report builder from html

---
+++ Core.Report/ReportGenerator.cs
@@ -0,0 +1,32 @@
+﻿using DinkToPdf;
+
+namespace Core.Report
+{
+    public static class ReportGenerator
+    {
+        public static byte[] GeneratePDF_from_HTML(string html)
+        {
+            var converter = new BasicConverter(new PdfTools());
+
+            var doc = new HtmlToPdfDocument
+            {
+                GlobalSettings =
+                {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4
+                },
+                Objects =
+                {
+                    new ObjectSettings
+                    {
+                        HtmlContent = html,
+                        WebSettings = { DefaultEncoding = "utf-8" }
+                    }
+                }
+            };
+
+            return converter.Convert(doc);
+        }
+    }
+}
