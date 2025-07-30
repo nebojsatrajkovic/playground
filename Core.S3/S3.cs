0a8af7139912481192aa00aad1b0b5a3e1719387
Nebojsa Trajkovic <55205314+nebojsatrajkovic@users.noreply.github.com>
2025-07-30 14:08:30 +0200

    implemented S3 client library

---
+++ Core.S3/S3.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Core.S3.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.S3
+{
+    public static class S3
+    {
+        /// <summary>
+        /// In order to use this S3 client developer must specify following environment variables:
+        /// a. S3_ACCESS_KEY_ID
+        /// b. S3_SECRET_ACCESS_KEY
+        /// c. S3_SERVICE_URL
+        /// d. S3_BUCKET_NAME
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IMvcBuilder UseCoreS3(this IMvcBuilder builder)
+        {
+            var S3_ACCESS_KEY_ID = Environment.GetEnvironmentVariable("S3_ACCESS_KEY_ID");
+            var S3_SECRET_ACCESS_KEY = Environment.GetEnvironmentVariable("S3_SECRET_ACCESS_KEY");
+            var S3_SERVICE_URL = Environment.GetEnvironmentVariable("S3_SERVICE_URL");
+            var S3_BUCKET_NAME = Environment.GetEnvironmentVariable("S3_BUCKET_NAME") ?? string.Empty;
+
+            S3Service.s3Client = new AmazonS3Client(new BasicAWSCredentials(S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY), new AmazonS3Config
+            {
+                ServiceURL = S3_SERVICE_URL
+            });
+
+            S3Service.s3BucketName = S3_BUCKET_NAME;
+
+            return builder;
+        }
+    }
+}
---
+++ Core.S3/Services/S3Service.cs
@@ -0,0 +1,112 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.S3.Services
+{
+    public static class S3Service
+    {
+        internal static IAmazonS3 s3Client;
+        internal static string s3BucketName;
+
+        public static async Task Upload(Stream stream, string keyPrefix, string keyFragment)
+        {
+            EnsureBucketExists();
+
+            var key = $"{keyPrefix}/{keyFragment}";
+
+            var request = new PutObjectRequest
+            {
+                BucketName = s3BucketName,
+                Key = key,
+                InputStream = stream,
+                DisablePayloadSigning = true
+            };
+
+            await s3Client.PutObjectAsync(request);
+        }
+
+        public static async Task Upload(IFormFile file, string keyPrefix, string keyFragment)
+        {
+            EnsureBucketExists();
+
+            var key = $"{keyPrefix}/{keyFragment}";
+
+            var request = new PutObjectRequest
+            {
+                BucketName = s3BucketName,
+                Key = key,
+                InputStream = file.OpenReadStream(),
+                DisablePayloadSigning = true
+            };
+            request.Metadata.Add("Content-Type", file.ContentType);
+            await s3Client.PutObjectAsync(request);
+        }
+
+        public static async Task<ListObjectsV2Response> List(string keyPrefix)
+        {
+            EnsureBucketExists();
+
+            var request = new ListObjectsV2Request
+            {
+                BucketName = s3BucketName,
+                Prefix = keyPrefix
+            };
+
+            return await s3Client.ListObjectsV2Async(request);
+        }
+
+        public static async void Delete(string keyPrefix, string keyFragment)
+        {
+            EnsureBucketExists();
+
+            var key = $"{keyPrefix}/{keyFragment}";
+
+            var deleteObjectRequest = new DeleteObjectRequest
+            {
+                BucketName = s3BucketName,
+                Key = key
+            };
+
+            var res = await s3Client.DeleteObjectAsync(deleteObjectRequest);
+        }
+
+        public static async Task<long?> CalculateStorage(string keyPrefix, string keyFragment)
+        {
+            EnsureBucketExists();
+
+            var key = $"{keyPrefix}/{keyFragment}";
+
+            var data = await s3Client.ListObjectsAsync(s3BucketName, key);
+
+            var size = data.S3Objects.Sum(x => x.Size); // in bytes
+
+            return size;
+        }
+
+        public static async void DeleteAllObjects(string keyPrefix, string keyFragment)
+        {
+            var key = $"{keyPrefix}/{keyFragment}";
+
+            var data = await s3Client.ListObjectsAsync(s3BucketName, key);
+
+            var keys = data.S3Objects.Select(x => x.Key).ToList();
+
+            foreach (var item in keys)
+            {
+                var deleteObjectRequest = new DeleteObjectRequest
+                {
+                    BucketName = s3BucketName,
+                    Key = item
+                };
+
+                var res = await s3Client.DeleteObjectAsync(deleteObjectRequest);
+            }
+        }
+
+        static async void EnsureBucketExists()
+        {
+            await s3Client.EnsureBucketExistsAsync(s3BucketName);
+        }
+    }
+}
