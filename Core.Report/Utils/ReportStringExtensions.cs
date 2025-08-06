e4a9aa4ae50f15f0066910a572e4b7414c4683bc
Nebojsa Trajkovic <55205314+nebojsatrajkovic@users.noreply.github.com>
2025-08-06 17:30:09 +0200

    added report template string extension methods

---
+++ Core.Report/Utils/ReportStringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Core.Report.Utils
+{
+    public static class ReportStringExtensions
+    {
+        public static string ReplaceTemplateValues(this string template, Dictionary<string, string?> replacements)
+        {
+            foreach (var kvp in replacements)
+            {
+                template = template.Replace($"${{{kvp.Key}}}", kvp.Value ?? string.Empty);
+            }
+
+            return template;
+        }
+    }
+}
