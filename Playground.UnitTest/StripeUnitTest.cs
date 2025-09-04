35c1163bd488c48df0a760d8e9e1cc47fe8e2c7c
Nebojsa Trajkovic <nebojsa.trajkovic92@gmail.com>
2025-09-04 11:39:32 +0200

    initialized unit test project

---
+++ Playground.UnitTest/StripeUnitTest.cs
@@ -0,0 +1,13 @@
+namespace Playground.UnitTest
+{
+    public class StripeUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = 2 + 3;
+
+            Assert.Equal(5, result);
+        }
+    }
+} 
